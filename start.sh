fa86d564d4e596bc7002b9daf2f8a071196d9741
robhinds <robhinds@gmail.com>
2017-08-13 10:05:09 +0100

    fixing sdk stuff so it actually works..

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sudo apt-get install -y git python-pip python-dev
 sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+sudo pip install --upgrade pip
 sudo pip install ansible
+ansible-playbook platbook.yml --sudo -K -c local -i "localhost,"
